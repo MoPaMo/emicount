9f6e5fbfc0c7af2e7185cb23a59e0cc8d3d326ba
MoPaMo <mpm1301@web.de>
2022-06-10 17:09:39 +0100

    Initial Commit

---
+++ Shared/emiCountApp.swift
@@ -0,0 +1,17 @@
+//
+//  emiCountApp.swift
+//  Shared
+//
+//  Created by Moritz Mockenhaupt on 10.06.22.
+//
+
+import SwiftUI
+
+@main
+struct emiCountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

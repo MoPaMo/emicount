81099298b8cc9c3c83c0d866a008e752fd02c483
MoPaMo <mpm1301@web.de>
2022-06-14 20:53:30 +0200

    start

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,13 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            VStack {
+                Text("Countily")
+                    .font(.title)
+                    .padding()
+                ProgressView(value: 0.75)
+                    .padding(.horizontal)
+                Text("8 Tage")
+                    .font(.subheadline)
+                
+                
+                
+                    
+                
+            }
+            .padding(.all)
+            Button("Datum ändern") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+        }.padding(.all)
+        
+        
     }
 }
+var days = 10;
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 8")
     }
 }
